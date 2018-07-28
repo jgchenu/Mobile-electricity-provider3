07001535797296c2749f65c3e80fdfd0d438c873
jgchenu <jgchenu@foxmail.com>
2018-07-29 01:19:25 +0800

    更新到了推荐商品的分页效果实现

--- src/containers/ShoppingMall.jsx
+++ src/containers/ShoppingMall.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import locationIcon from "../assets/images/location.png";
 import url from "../serviceAPI.config";
 import classes from "../assets/style/ShoppingMall.scss";
-import { Button, Carousel, WingBlank } from "antd-mobile";
+import { Button, Carousel } from "antd-mobile";
+import sortArr from "../utils/sortArr";
+import { toMoney } from "../utils/filter";
 // import axios from "axios";
 class ShoppingMall extends React.Component {
   constructor() {
@@ -21,7 +23,6 @@ class ShoppingMall extends React.Component {
     console.log(this);
   }
   componentDidMount() {
-    const _this = this;
     React.$axios({
       type: "get",
       url: url.getShoppingMallInfo
@@ -29,17 +30,18 @@ class ShoppingMall extends React.Component {
       .then(res => {
         console.log(res);
         if (res.status === 200) {
-          _this.setState({
+          this.setState({
             category: res.data.data.category,
             adBanner: res.data.data.advertesPicture.PICTURE_ADDRESS,
             bannerPicArr: res.data.data.slides,
-            recommendGoods: res.data.data.recommend,
+            recommendGoods: sortArr(res.data.data.recommend),
             floor1: res.data.data.floor1,
             floor2: res.data.data.floor2,
             floor3: res.data.data.floor3,
             floorName: res.data.data.floorName,
             hotGoods: res.data.data.hotGoods
           });
+          console.log(this.state.recommendGoods);
         }
       })
       .catch(err => {
@@ -70,10 +72,92 @@ class ShoppingMall extends React.Component {
         </div>
         {/* swiper */}
 
-        <div className="swiperArea">
-          {/* <WingBlank>
-            <Carousel infinite />
-          </WingBlank> */}
+        <div className={classes.swiperArea}>
+          <Carousel autoplay infinite>
+            {this.state.bannerPicArr.map(val => (
+              <img
+                key={val.goodsId}
+                src={val.image}
+                alt="轮播图"
+                style={{
+                  width: "100%",
+                  verticalAlign: "top",
+                  height: this.state.imgHeight
+                }}
+                onLoad={() => {
+                  // fire window resize event to change height
+                  window.dispatchEvent(new Event("resize"));
+                  this.setState({ imgHeight: "8rem" });
+                }}
+              />
+            ))}
+          </Carousel>
+        </div>
+
+        {/* <!-- type-bar --> */}
+
+        <div className={classes.typeBar}>
+          {this.state.category.map((cate, index) => {
+            return (
+              <div key={index}>
+                <img src={cate.image} alt="类型图" />
+                <span>{cate.mallCategoryName}</span>
+              </div>
+            );
+          })}
+        </div>
+
+        {/* addBanner */}
+        <div>
+          <img src={this.state.adBanner} alt="adbanner" width="100%" />
+        </div>
+        {/* recommend goods */}
+        <div className={classes.recommendArea}>
+          <div className={classes.recommendTitle}>商品推荐</div>
+          <div>
+            <Carousel
+              dots={false}
+              cellSpacing={10}
+              slideWidth={0.8}
+              autoplay={false}
+              infinite
+              afterChange={index => this.setState({ slideIndex: index })}
+            >
+              {this.state.recommendGoods.map((val, index) => {
+                return (
+                  <div
+                    className={classes.recommendBody}
+                    key={index}
+                    style={{
+                      position: "relative",
+                      top: this.state.slideIndex === index ? -10 : 10,
+                      oxShadow: "2px 1px 1px rgba(0, 0, 0, 0.2)",
+                      height: this.state.recommendHeight
+                    }}
+                    onLoad={() => {
+                      // fire window resize event to change height
+                      window.dispatchEvent(new Event("resize"));
+                      this.setState({
+                        recommendHeight: "auto"
+                      });
+                    }}
+                  >
+                    {val.map((item, num) => {
+                      return (
+                        <div key={num} className={classes.recommendItem}>
+                          <img src={item.image} width="80%" alt="推荐商品" />
+                          <div>{item.goodsName}</div>
+                          <div>
+                            ￥{toMoney(item.price)}(￥{toMoney(item.mallPrice)})
+                          </div>
+                        </div>
+                      );
+                    })}
+                  </div>
+                );
+              })}
+            </Carousel>
+          </div>
         </div>
       </div>
     );
